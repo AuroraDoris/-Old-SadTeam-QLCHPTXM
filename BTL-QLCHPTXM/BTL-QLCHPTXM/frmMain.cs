88d5aa0286de3fcba793c6776a168cb5b03b39a8
AuroraDoris <nguyenmtmcuong@gmail.com>
2016-12-04 10:51:54 +0700

    Tao frmMain

---
+++ BTL-QLCHPTXM/BTL-QLCHPTXM/frmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BTL_QLCHPTXM
+{
+    public partial class frmMain : Form
+    {
+        string cnStr;
+        SqlConnection cn;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            cnStr = "Server = .; Database = DatabaseQLCHPTXM; Integrated Security = true";
+            cn = new SqlConnection(cnStr);
+        }
+    }
+}
